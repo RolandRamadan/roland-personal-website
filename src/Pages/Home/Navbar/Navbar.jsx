a6b99a6ae24a4641c6b7b9dd28817ec7899972be
Roland Ramadan <129726986+RolandRamadan@users.noreply.github.com>
2024-11-25 02:17:47 +0700

    fix: navbar broken

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 import "./Navbar.css";
 export default function Navbar() {
     const [navActive, setNavActive] = useState(false);
